387e4c84c227297ba52e77ae6fdb93cf821174d0
commander <bprim@wsu.edu>
2014-06-24 21:25:13 -0700

    ApocalypseGuessGame

---
+++ js/ApocalypseGit.js
@@ -0,0 +1,31 @@
+<script>
+  var random, result;
+  var findDifference = function(answer, guess) {
+    if (answer > guess) {
+      return answer - guess;
+    }
+    else {
+      return guess - answer;
+    }
+  }
+ 
+  var guessingApocalypse = function(solution) {
+    var predict, difference;
+    predict = prompt("The zombie apocalypse will occurr on day: ");
+    difference = findDifference(predict, solution );
+ 
+    if (parseInt(predict) === solution) {
+      console.log("It's happening...");
+      return "You're right! - WARN humanity!";
+    }
+    else {
+      return difference + " zombies will attack you, predict again";
+    }
+  }
+ 
+  random = Math.floor(Math.random() * 365)
+  for (tries = 1; tries < 4; tries++) {
+    result = guessingApocalypse(random)
+    alert(result);
+  }
+</script>
